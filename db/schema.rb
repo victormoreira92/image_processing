8e16b17355ce9d7a7fe842b38a166e9ec2849187
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-11-12 17:46:44 -0300

    feat: Add JIT to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_11_162829) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_12_180056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_11_162829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+    t.string "jti"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
