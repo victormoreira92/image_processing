098ea42a42efbf3a95275b4499f23690bf0804ef
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-11-12 18:39:25 -0300

    test: Image tests

---
+++ spec/models/image_spec.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_images_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+require 'rails_helper'
+
+RSpec.describe Image, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
